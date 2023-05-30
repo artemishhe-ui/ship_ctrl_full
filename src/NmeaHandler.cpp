2080c8451833163c6074f8595b718f85844f002e
AresU <user35@mail.ru>
2023-05-30 23:43:30 +0300

    none needed

---
+++ src/NmeaHandler.cpp
@@ -0,0 +1,9 @@
+#include "NmeaHandler.h"
+
+/*/
+std::string NmeaHandler::pre_read(std::string msg)
+{
+    // TODO: add regex read to array? aka [0] = "type" [*] = data
+    return std::string();
+}
+*/
